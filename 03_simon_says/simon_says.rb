8538e92609315449dd2a175d15e371eff911618f
Luke Bock <lukebock6@gmail.com>
2016-11-21 14:31:59 -0800

    trynna get the 'qu' working

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -16,3 +16,38 @@ def repeat(x,y=2)
 	end
 	string.strip
 end
+
+def start_of_word(word, count)
+	i = 0
+	s = ""
+	while count > i
+		s=s+word[i]
+		i=i+1
+	end
+	s
+end
+
+def first_word(string)
+	string.split(" ").first
+end
+
+def titleize(str)
+	words = str.split(" ")
+	str = ""
+	words.each_with_index do |word, index|
+		if (word=="and") || (word=="over")
+			str = str+word+" "
+		elsif word=="the"
+			if index==0
+				ns = word.slice(0,1).capitalize + word.slice(1..-1)
+				str = str+ns+" "
+			else
+				str = str+word+" "
+			end
+		else
+			ns = word.slice(0,1).capitalize + word.slice(1..-1)
+			str = str+ns+" "
+		end
+	end
+	str.strip
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
-#write your code here
+def translate(phrase)
+	vowel = ['a','e','i','o','u']
+	ret = ''
+	words = phrase.split(' ')
+	words.each do |word|
+		if vowel.include? word.slice(0)
+			word=word+"ay"
+			ret<<word		
+		elsif not vowel.include? word.slice(1)
+			if not vowel.include? word.slice(2)
+				word = word[3...word.length]+word[0...3]+'ay'
+				ret<<word
+			else
+				word = word[2...word.length]+word[0...2]+'ay'
+				ret<<word
+			end
+		else
+			letters = word.split("")
+			letters = letters.rotate
+			word = letters.join("")+"ay"
+			ret<<word
+		end
+		ret=ret+' '
+	end
+	return ret.strip
+end
