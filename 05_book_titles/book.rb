ebe097c023d400033c437a523138e44e27f3e6b3
Luke Bock <lukebock6@gmail.com>
2016-11-22 09:11:41 -0800

    book titles are easy

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title=(newTitle)
+		words = newTitle.split(" ")
+		smallWords = %w(the a an and in of to)
+		words = words.map do |word|
+			if smallWords.include? (word)
+				word
+			else
+				word.capitalize
+			end
+		end
+		words[0] = words[0].capitalize
+		@title=words.join(" ")	
+	end	
 end
