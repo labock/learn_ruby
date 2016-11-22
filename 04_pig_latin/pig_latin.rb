5b7df5feecdf44edaf672e54f131046f8a6f40c4
Luke Bock <lukebock6@gmail.com>
2016-11-22 08:13:57 -0800

    simon says son

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -10,10 +10,16 @@ def translate(phrase)
 			if not vowel.include? word.slice(2)
 				word = word[3...word.length]+word[0...3]+'ay'
 				ret<<word
+			elsif word.slice(1)=="q"&&word.slice(2)=="u"
+				word = word[3...word.length]+word[0...3]+'ay'
+				ret<<word
 			else
 				word = word[2...word.length]+word[0...2]+'ay'
 				ret<<word
 			end
+		elsif word.slice(0)=="q"&&word.slice(1)=="u"
+			word = word[2...word.length]+word[0...2]+'ay'
+			ret<<word
 		else
 			letters = word.split("")
 			letters = letters.rotate
